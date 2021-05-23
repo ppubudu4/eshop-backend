d02e27c6010f6050186cc61470470b08cb51a1e7
@ppubudu4 <ppubudu4@gmail.com>
2021-05-23 21:52:58 +0530

    order edit and delete

--- routers/orders.js
+++ routers/orders.js
@@ -12,6 +12,7 @@ router.get(`/`, async (req, res) => {
   }
   res.send(orderList);
 });
+
 router.get(`/:id`, async (req, res) => {
   const order = await Order.findById(req.params.id)
     .populate('user', 'name')
@@ -60,6 +61,39 @@ router.post('/', async (req, res) => {
 
   if (!order) return res.status(404).send('The order cannot be created!');
   res.send(order);
+});
+
+router.put('/:id', async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    { new: true }
+  );
+  if (!order) return res.status(404).send('The order cannot be Updated!');
+  res.send(order);
+});
+
+router.delete('/:id', (req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res
+          .status(200)
+          .json({ success: true, message: 'The order is deleted' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Order not found' });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
 });
 
 module.exports = router;
