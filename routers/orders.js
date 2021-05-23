2b81510c648be56a5c2cb4a2cc34708636773edf
@ppubudu4 <ppubudu4@gmail.com>
2021-05-23 22:21:03 +0530

    total price calculate

--- routers/orders.js
+++ routers/orders.js
@@ -44,6 +44,22 @@ router.post('/', async (req, res) => {
   );
   const orderItemsIdsResovlved = await orderItemsIds;
 
+  //calculate price
+  const totalPrices = await Promise.all(
+    orderItemsIdsResovlved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        'product',
+        'price'
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  console.log(totalPrices);
+  //combind array
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
   let order = new Order({
     orderItems: orderItemsIdsResovlved,
     shippingAddress1: req.body.shippingAddress1,
@@ -53,7 +69,7 @@ router.post('/', async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user,
   });
 
